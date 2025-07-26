7a12ba53359b31a65d4d6ce880b83fbe5f4877b2
jerebrrs <kevinbrrs96@gmail.com>
2025-07-26 18:51:44 -0300

    Ejercicio 1.3/2.1/2.27

---
+++ Ejercicio_2.1/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Ejercicio_21
+{
+    class Program
+    {
+        static int[] num = new int[100];
+        static void Main(string[] args)
+        {
+            Console.Write("");
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int numRnadom = rnd.Next(0, 200);
+                num[i] = numRnadom;
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($" - {i + 1}:{num[i]}");
+            }
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Listado ordenado de Mayor a menor:");
+
+            ordenarBurbuja(num);
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($" - {num[i]}");
+            }
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Listado ordenada Quicksort de Menor a mayor:");
+
+            QuickSort(num, 0, num.Length - 1);
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($" - {num[i]}");
+            }
+
+            int numBuscado = rnd.Next(0, 200);
+
+            Console.WriteLine($"Numero a buscar: {numBuscado}");
+            int numEncontrado = Busqueda(num, numBuscado);
+
+            if (numEncontrado != -1)
+            {
+
+                Console.WriteLine($"El numero se encuentra en la posicion: {numEncontrado}");
+            }
+            else
+            {
+                Console.WriteLine($"El numero no se encontro.");
+            }
+
+            Console.ReadKey();
+        }
+        #region Metodos de ordenamiento
+        static void ordenarBurbuja(int[] vector)
+        {
+            for (int i = 0; i < vector.Length - 1; i++)
+            {
+                for (int j = i + 1; j < vector.Length; j++)
+                {
+                    if (vector[i] < vector[j])
+                    {
+                        int aux = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = aux;
+                    }
+
+                }
+
+            }
+        }
+
+        static void QuickSort(int[] vector, int inicio, int fin)
+        {
+            if (inicio >= fin)
+                return;
+
+            int pivote = vector[inicio];
+            int izquierda = inicio + 1;
+            int derecha = fin;
+
+            while (izquierda <= derecha)
+            {
+                while (izquierda <= fin && vector[izquierda] < pivote)
+                    izquierda++;
+
+                while (derecha > inicio && vector[derecha] >= pivote)
+                    derecha--;
+
+                if (izquierda < derecha)
+                {
+                    int temp = vector[izquierda];
+                    vector[izquierda] = vector[derecha];
+                    vector[derecha] = temp;
+                }
+            }
+
+            // Intercambio del pivote con el valor en 'derecha'
+            vector[inicio] = vector[derecha];
+            vector[derecha] = pivote;
+
+            // Llamadas recursivas
+            QuickSort(vector, inicio, derecha - 1);
+            QuickSort(vector, derecha + 1, fin);
+        }
+        #endregion
+
+        static int Busqueda(int[] vector, int vBuscado)
+        {
+            int pos = -1;
+            int cont = 0;
+            while (pos == -1 && cont < vector.Length)
+            {
+                if (vector[cont] == vBuscado)
+                {
+                    pos = cont;
+                }
+                cont++;
+            }
+            return pos;
+
+        }
+    }
+
+}
---
+++ Ejercicio_2.2/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Ejercicio_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vNum = new int[100];
+            Random rnd = new Random();
+            string[] nombres = { "Noemí", "Noelía", "Andrés", "Emilio", "Norberto", "Estefanía", "Daniela", "Valeria" };
+            string[] nombresAleatorio = new string[100];
+
+            for (int i = 0; i < vNum.Length; i++)
+            {
+                vNum[i] = rnd.Next(1, 100);
+                int numRnd = rnd.Next(0, nombres.Length);
+                nombresAleatorio[i] = nombres[numRnd];
+            }
+            Console.WriteLine("=== Listado original ===");
+            MostrarListado(vNum, nombresAleatorio);
+
+            int[] vNumBurbuja = new int[vNum.Length];
+            string[] nombresBurbuja = new string[nombresAleatorio.Length];
+
+            int[] vNumQuick = new int[vNum.Length];
+            string[] nombresQuick = new string[nombresAleatorio.Length];
+
+            for (int i = 0; i < vNum.Length; i++)
+            {
+                vNumBurbuja[i] = vNum[i];
+                nombresBurbuja[i] = nombresAleatorio[i];
+
+                vNumQuick[i] = vNum[i];
+                nombresQuick[i] = nombresAleatorio[i];
+            }
+
+            Burbuja(vNumBurbuja, nombresBurbuja);
+            Console.WriteLine("\n=== Ordenado con Burbuja ===");
+            MostrarListado(vNumBurbuja, nombresBurbuja);
+
+
+            QuickSort(vNumQuick, nombresQuick, 0, vNumQuick.Length - 1);
+            Console.WriteLine("\n=== Ordenado con QuickSort ===");
+            MostrarListado(vNumQuick, nombresQuick);
+        }
+        static void MostrarListado(int[] numeros, string[] nombres)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Inidice :{i} , Numero: {numeros[i]} , Nombre: {nombres[i]} .");
+
+            }
+        }
+
+        #region Ordenamiento Burubuja Quiksort
+        static void Burbuja(int[] numeros, string[] nombres)
+        {
+            int n = numeros.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numeros[j] > numeros[j + 1])
+                    {
+                        // Intercambiar números
+                        int tempNum = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = tempNum;
+
+                        // Intercambiar nombres correspondientes
+                        string tempNom = nombres[j];
+                        nombres[j] = nombres[j + 1];
+                        nombres[j + 1] = tempNom;
+                    }
+                }
+            }
+        }
+
+        static void QuickSort(int[] numeros, string[] nombres, int inc, int fin)
+        {
+            if (inc < fin)
+            {
+                int pivote = Particionar(numeros, nombres, inc, fin);
+                QuickSort(numeros, nombres, inc, pivote - 1);
+                QuickSort(numeros, nombres, pivote + 1, fin);
+            }
+        }
+
+        private static int Particionar(int[] numeros, string[] nombres, int inc, int fin)
+        {
+            int pivote = numeros[fin];
+            int cont = inc - 1;
+
+            for (int i = inc; i < fin; i++)
+            {
+                if (numeros[i] <= pivote)
+                {
+                    cont++;
+
+                    int tempNum = numeros[cont];
+                    numeros[cont] = numeros[i];
+                    numeros[i] = tempNum;
+
+                    // Intercambiar nombres
+                    string tempNom = nombres[cont];
+                    nombres[cont] = nombres[i];
+                    nombres[i] = tempNom;
+                }
+            }
+            int tempNumPivot = numeros[cont + 1];
+            numeros[cont + 1] = numeros[fin];
+            numeros[fin] = tempNumPivot;
+
+            string tempNomPivot = nombres[cont + 1];
+            nombres[cont + 1] = nombres[fin];
+            nombres[fin] = tempNomPivot;
+
+            return cont + 1;
+        }
+        #endregion
+    }
+
+}
